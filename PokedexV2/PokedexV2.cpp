88b73b9fe75b4969d5302a9c8d354f6a18f0cb1f
Maxpro-PC\Maxpro <maxdietz@hotmail.com>
2018-07-07 22:18:29 -0700

    Added debug code in main() related to getting videomodes in SFML

--- PokedexV2/PokedexV2.cpp
+++ PokedexV2/PokedexV2.cpp
@@ -1258,6 +1258,23 @@ int main(int argc, char* argv[])
 	//shape.setFillColor(sf::Color::Green);
 
 
+	// Display the list of all the video modes available for fullscreen
+	std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
+	for (std::size_t i = 0; i < modes.size(); ++i)
+	{
+		sf::VideoMode mode = modes[i];
+		std::cout << "Mode #" << i << ": "
+			<< mode.width << "x" << mode.height << " - "
+			<< mode.bitsPerPixel << " bpp" << std::endl;
+	}
+	//// Create a window with the same pixel depth as the desktop
+	//sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+	//window.create(sf::VideoMode(1024, 768, desktop.bitsPerPixel), "SFML window");
+
+
+
+
+
 	sf::RenderWindow window;
 
 	sf::Vector2i centerWindow((sf::VideoMode::getDesktopMode().width / 2) - 445, (sf::VideoMode::getDesktopMode().height / 2) - 480);
